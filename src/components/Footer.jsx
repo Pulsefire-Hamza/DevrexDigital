1bbab41ac46b42dc5096d48bcdf37957c6dadb7c
Hamza Tariq <140837284+Pulsefire-Hamza@users.noreply.github.com>
2024-10-19 03:08:54 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebookF, FaInstagram, FaLinkedin, FaTimes } from 'react-icons/fa';
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa'; // Import necessary icons
 
 const Footer = () => {
   return (
@@ -33,27 +33,12 @@ const Footer = () => {
 
         {/* Social Icons */}
         <div className="flex space-x-4">
-          <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-            <FaFacebookF className="text-blue-600 hover:text-blue-700" /> {/* Facebook color */}
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+          <a href="https://www.instagram.com/devrexdigital" target="_blank" rel="noreferrer">
             <FaInstagram className="text-pink-600 hover:text-pink-700" /> {/* Instagram color */}
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
-            <FaLinkedin className="text-blue-700 hover:text-blue-800" /> {/* LinkedIn color */}
+          <a href="https://wa.me/996702473024" target="_blank" rel="noreferrer"> {/* WhatsApp link */}
+            <FaWhatsapp className="text-green-500 hover:text-green-600" /> {/* WhatsApp color */}
           </a>
-          <a href="https://www.fiverr.com" target="_blank" rel="noreferrer">
-            {/* Fiverr SVG Icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              className="h-6 w-6 text-green-500 hover:text-green-600"
-              fill="currentColor"
-            >
-              <path d="M11.254 0H4.746C3.09 0 1.87.597.972 1.513A4.004 4.004 0 000 4.992v6.016A4.004 4.004 0 00.972 12.487C1.87 13.403 3.09 14 4.746 14h6.508c1.656 0 2.876-.597 3.774-1.513C15.854 12.403 16 11.555 16 10.985V5.015c0-.57-.146-1.418-.472-1.993C14.63.597 13.41 0 11.754 0zM8 12.517a3.637 3.637 0 01-3.636-3.637c0-.024 0-.049.002-.073h1.3a2.36 2.36 0 002.336 2.336c.003.024.005.049.005.073zm1.363-2.49h-1.728c-.128.243-.308.467-.546.646V8.59h2.328V7.643h-2.328V6.45h2.728V5.498h-2.972V5h1.642c.126.012.254.016.387.046.088.026.176.056.261.084v3.036c0 .4-.111.805-.303 1.168-.14.264-.334.507-.565.72z" />
-            </svg>
-          </a>
-          <FaTimes className="text-xl cursor-pointer" />
         </div>
       </div>
     </footer>
